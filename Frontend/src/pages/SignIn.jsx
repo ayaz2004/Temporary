b1d8b37e8fc33571df9b5ec0d0f6e452355b4264
ayaz2004 <ayaznazir03@gmail.com>
2025-01-26 00:55:36 +0530

    Add Redux for user authentication; implement sign-in functionality and update SignIn component

--- Frontend/src/pages/SignIn.jsx
+++ Frontend/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -13,11 +19,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields.");
+      return dispatch(signInFailure("Please fill all the fields"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -25,15 +30,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -43,12 +48,13 @@ export default function SignIn() {
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Temporary
+              Sahand's
             </span>
+            Blog
           </Link>
           <p className="text-sm mt-5">
-            This is a temporary project. You can sign in with your email and
-            password or with Google.
+            This is a demo project. You can sign in with your email and password
+            or with Google.
           </p>
         </div>
         {/* right */}
@@ -58,7 +64,7 @@ export default function SignIn() {
             <div>
               <Label value="Your email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="name@company.com"
                 id="email"
                 onChange={handleChange}
@@ -68,7 +74,7 @@ export default function SignIn() {
               <Label value="Your password" />
               <TextInput
                 type="password"
-                placeholder="***********"
+                placeholder="**********"
                 id="password"
                 onChange={handleChange}
               />
@@ -89,7 +95,7 @@ export default function SignIn() {
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
-            <span>Don't have an account?</span>
+            <span>Dont Have an account?</span>
             <Link to="/sign-up" className="text-blue-500">
               Sign Up
             </Link>
